febfed07b357315320a91d705483356a588891e7
y-shafi <yshafi22@gmail.com>
2021-08-19 12:23:12 +0100

    ..

---
+++ app.js
@@ -0,0 +1,19 @@
+const shafisFunction = require("./shafisFunction")
+const _ = require("lodash");
+
+console.log(_)
+
+// const bananas = require("./shafisFunction")
+// const os = require("os")
+// const fs = require("fs")
+
+// let userDetails = os.userInfo().username
+
+// fs.appendFile('oh-hi.txt', `Hello ${userDetails}`, (err) => {
+//     if (err) {
+//         console.log('oops');
+//     }
+// });
+
+// console.log(bananas.add(2, 2))
+// console.log(bananas.multiply(5, 10))
---
+++ shafisFunction.js
@@ -0,0 +1,16 @@
+const add = (num1, num2) => {
+    return num1 + num2
+}
+const multiply = (num3, num4) => {
+    return num3 * num4
+}
+
+const bananas = 'shafisFunction.js'
+
+module.exports = {
+    add: add,
+    multiply: multiply,
+    name: bananas,
+    desc: "these functions shall help you",
+    noOfFunctions: 2,
+}
